3f23404464cdacfe22b5c4838fa728bd5f2dd6fc
Galushko Andrey <createsite2016@gmail.com>
2021-06-04 16:03:04 +0300

    Добавил роутин для аудентификации пользователя

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,9 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+
+Route::get(
+    'user/login',
+    [LoginController::class, 'index']
+)->name('page.user.login.index');
