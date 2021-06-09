2f6064b9abe99b3044aa2fc7adeadf12e1917934
Galushko Andrey <createsite2016@gmail.com>
2021-06-09 18:06:16 +0300

    Изменил аватарку в личном кабинете

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -93,7 +93,7 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                    <img src="dist/img/avatar5.png" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">{{ Auth::user()->name }}</a>
@@ -105,7 +105,7 @@
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                     <li class="nav-item">
                         <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-th"></i>
+                            <i class="fas fa-briefcase"></i>
                             <p>
                                 Задачи
                             </p>
@@ -113,9 +113,17 @@
                     </li>
                     <li class="nav-item">
                         <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-th"></i>
+                            <i class="fas fa-user-friends"></i>
                             <p>
                                 Сотрудники
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('user.logout') }}" class="nav-link">
+                            <i class="fas fa-door-open"></i>
+                            <p>
+                                Выход
                             </p>
                         </a>
                     </li>
@@ -161,7 +169,7 @@
     <footer class="main-footer">
         <!-- To the right -->
         <div class="float-right d-none d-sm-inline">
-            Разработанно по индивидульному заказу
+            Разработанно по индивидуальному заказу
         </div>
         <!-- Default to the left -->
         <strong>OZBERG &copy; 2021</strong>
