a489c7c041a3279f35fd6af1d7d0b5ae32146bec
Galushko Andrey <createsite2016@gmail.com>
2021-06-09 18:06:37 +0300

    сделал маршруты в одну строчку (так пизже)

--- routes/web.php
+++ routes/web.php
@@ -8,28 +8,22 @@ use Illuminate\Support\Facades\Route;
 // -----------------------------------------------------------------------------
 //  Аудентификация
 // -----------------------------------------------------------------------------
-Route::get('user/login/', [LoginController::class, 'index'])
-    ->name('user.login.index');
-Route::post('user/login/', [LoginController::class, 'store'])
-    ->name('user.login.store');
+Route::get('user/login/', [LoginController::class, 'index'])->name('user.login.index');
+Route::post('user/login/', [LoginController::class, 'store'])->name('user.login.store');
 
 // -----------------------------------------------------------------------------
 //  Регистрация
 // -----------------------------------------------------------------------------
-Route::get('user/registration/', [RegistrationController::class, 'index'])
-    ->name('user.registration.index');
-Route::post('user/registration', [RegistrationController::class, 'store'])
-    ->name('user.registration.store');
+Route::get('user/registration/', [RegistrationController::class, 'index'])->name('user.registration.index');
+Route::post('user/registration', [RegistrationController::class, 'store'])->name('user.registration.store');
 
 // -----------------------------------------------------------------------------
 //  Выход
 // -----------------------------------------------------------------------------
-Route::get('user/logout/', [LoginController::class, 'logout'])
-    ->name('user.logout');
+Route::get('user/logout/', [LoginController::class, 'logout'])->name('user.logout');
 
 // -----------------------------------------------------------------------------
 //  Пользователь
 // -----------------------------------------------------------------------------
-Route::get('user/', [UserController::class, 'index'])
-    ->middleware('auth')
-    ->name('user.index');
+Route::get('user/', [UserController::class, 'index'])->middleware('auth')->name('user.index');
+Route::get('user/', [UserController::class, 'index'])->middleware('auth')->name('user.index');
